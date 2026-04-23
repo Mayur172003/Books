6ff4ccb457d8ddb74d212379bce3b31d2192afe3
mayur.gharat <mayur.gharat@instinctinnovations.com>
2026-04-24 01:34:41 +0530

    chore: update surpriseQuotes.js by removing outdated quotes and adding new ones

--- src/data/surpriseQuotes.js
+++ src/data/surpriseQuotes.js
@@ -4,66 +4,6 @@
  */
 export const SURPRISE_QUOTES = [
   {
-    text: 'Once you learn to read, you will be forever free.',
-    book: 'Narrative of the Life of Frederick Douglass',
-    author: 'Frederick Douglass',
-  },
-  {
-    text: 'Reading is to the mind what exercise is to the body.',
-    book: 'The Tatler',
-    author: 'Joseph Addison',
-  },
-  {
-    text: 'The more that you read, the more things you will know.',
-    book: 'I Can Read With My Eyes Shut!',
-    author: 'Dr. Seuss',
-  },
-  {
-    text: 'Fill your house with stacks of books, in all the crannies and all the nooks.',
-    book: 'I Can Read With My Eyes Shut!',
-    author: 'Dr. Seuss',
-  },
-  {
-    text: 'I find television very educating. Every time somebody turns on the set, I go in the other room and read a book.',
-    book: 'Attributed quip',
-    author: 'Groucho Marx',
-  },
-  {
-    text: 'Let us read, and let us dance; these two amusements will never do any harm to the world.',
-    book: 'Voltaire in exile',
-    author: 'Voltaire',
-  },
-  {
-    text: 'A word after a word after a word is power.',
-    book: 'Spelling',
-    author: 'Margaret Atwood',
-  },
-  {
-    text: 'Books are a uniquely portable magic.',
-    book: 'On Writing',
-    author: 'Stephen King',
-  },
-  {
-    text: 'There is no enjoyment like reading!',
-    book: 'Pride and Prejudice',
-    author: 'Jane Austen',
-  },
-  {
-    text: 'Reading gives us someplace to go when we have to stay where we are.',
-    book: 'City Aphorisms',
-    author: 'Mason Cooley',
-  },
-  {
-    text: 'Today a reader, tomorrow a leader.',
-    book: 'Widely quoted',
-    author: 'Margaret Fuller',
-  },
-  {
-    text: 'She is too fond of books, and it has turned her brain.',
-    book: 'Little Women',
-    author: 'Louisa May Alcott',
-  },
-  {
     text: 'Never trust anyone who has not brought a book with them.(Except me😁)',
     book: 'Horseradish',
     author: 'Lemony Snicket',
@@ -72,5 +12,20 @@ export const SURPRISE_QUOTES = [
     text: 'You can never get a cup of tea large enough or a book long enough to suit me.',
     book: 'On Stories',
     author: 'C.S. Lewis',
+  },
+  {
+    text: 'Think before you speak. Read before you think.',
+    book: 'Social Studies',
+    author: 'Fran Lebowitz',
+  },
+  {
+    text: "Classic: a book which people praise and don't read. (So... you're allowed to skip the boring bits. I won't tell.)",
+    book: 'Following the Equator',
+    author: 'Mark Twain',
+  },
+  {
+    text: 'If loving books is a superpower, you are over here saving worlds. I am in the background holding the door and your second coffee.',
+    book: 'Sidekick energy',
+    author: 'Happy to cheer from the non-reader section',
   },
 ]
